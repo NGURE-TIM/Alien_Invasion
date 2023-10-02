0d1cb0fbed3c1931b483bb02b86b1d7e86a63f0f
NGURE-TIM <kangushtim@gmail.com>
2023-10-02 12:23:21 +0300

    created bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings 
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     '''Overall class to manage game assets and behaviours'''
@@ -15,6 +16,7 @@ class AlienInvasion:
         self.bg_color=(230,230,230)
         pygame.display.set_caption('Alien Invasion')
         self.ship=Ship(self)
+        self.bullets=pygame.sprite.Group()
     
     def _check_events(self):
          
@@ -27,22 +29,37 @@ class AlienInvasion:
                           self.ship.moving_right=True
                      elif event.key==pygame.K_LEFT:
                           self.ship.moving_left=True
-                     elif event.key==pygame.K-q:
+                     elif event.key==pygame.K_q:
                           sys.exit()
+                     elif event.key==pygame.K_SPACE:
+                          self._fire_bullet()
 
                 elif event.type==pygame.KEYUP:
                      if event.key == pygame.K_RIGHT:
                           self.ship.moving_right=False
                      elif event.key==pygame.K_LEFT:
                           self.ship.moving_left=False
+    def _fire_bullet(self):
+         '''Create a new bullet and add it to bullets'''
+         new_bullet=Bullet(self)
+         self.bullets.add(new_bullet)
+         
 
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            
             self.screen.fill(self.bg_color)
             self.ship.blitme()
+            for bullet in self.bullets.sprites():
+              bullet.draw_bullet()
+            for bullet in self.bullets.copy():
+                 if bullet.rect.bottom <= 0 :
+                      self.bullets.remove(bullet)
+            print(len(self.bullets))
             pygame.display.flip()
     
 if __name__ == '__main__':
